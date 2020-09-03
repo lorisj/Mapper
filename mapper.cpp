577d36190ae1e0531d2887df91ab0efbd28aeef2
Loris <loris@kalifornia.home>
2020-09-03 15:26:55 -0400

    Rebuild of mapper.h and mapper.cpp

--- mapper.cpp
+++ mapper.cpp
@@ -1,150 +1,74 @@
 #include "mapper.h"
-
 using namespace std;
+//gets the name of the wifi card (will always pick the first one (0 then 1 then 2...))
+string getWifiCard(){
+    string command = "ifconfig ";
+    string tmpflname = "DeleteMe.txt";
+    command += " > " + tmpflname;
+    system(command.c_str()); // cmd > Deleteme.txt
+    string out;
+    string temp;
+    ifstream fin(tmpflname);
+    fin >> temp;
+    while(fin >> temp && temp.substr(0,4) != "wlan");
+    command = "rm -f " + tmpflname;
+    system(command.c_str());
+    return temp.substr(0, temp.length() - 1); // Removes the :
+}//DT
 
-
-void setup(){
-    system("clear");
-    user = getCommandOutput("whoami");
-    make_known_map();// initializes (std::map<string,string> devices)
-}
-
-void cleanup(){
-    sudo("rm -f " +resc + "/stored/out* ");
-    string wcard = getWifiCard();
-    if (wcard.substr(wcard.size() - 3) == "mon"){
-        sudo("airmon-ng stop " + getWifiCard());
-    } else{
-        cout << "Card aleready not in mode";
-    }
-}
-
-
-//Gets password from the user
-void update_password(){
-    cout<<"ENTER SUDO PASSWORD FOR ACCOUNT \"" << user <<"\":";
-    cin >> password;
-    system("clear");
-}
-
-int finish(const int  & ercode){
-    cleanup();
-    return ercode;
-}
-void airodump_read(map<string, int > & scanned_devices, ifstream & outfile){
-    while(outfile){
-        string temp;
-        string trash;
-        outfile >> temp; // Station MAC,
-
-        // First time seen,
-        outfile >> trash; // date
-        outfile >> trash; // time
-
-        // Last time seen
-        outfile >> trash; // date
-        outfile >> trash; // time
-
-        outfile >> trash; //-Power,
-
-        //Takes the - sign and the comma out of trash and puts it into scanned_devices:
-        if (!temp.empty() && !trash.empty()){
-            scanned_devices[temp.substr(0, temp.size() - 1)] =
-                    stoi(trash.substr(1, trash.length() -1) );
-        }
-
-        //Continues the ifstream until a new line is present.
-        getline(outfile, trash);
-    }
-
-}
-bool check_runtime(){
-    string unit = runTime.substr(runTime.length()-1);
-    if ( !(unit == "m" || unit == "s" || unit == "h" )) return false;
-    for (size_t i = 0; i < runTime.length() - 1; i++)
-        if (isdigit(runTime[i]) == 0)
-            return false;
-    return true;
-}
-
-
-int main(int argc, char** argv){
-    //SETUP :: \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\;
-
-    setup();
-
-
-
-    //START USER INPUT: ------------------------------------------------------------------------------------------------
-    if (argc >1) {
-        password = *(argv + 1); //can take password as argument
-        if (argc == 3) {
-            runTime = toLowerCase(string (argv[2]) ); // sets second argument to be runtime
-            if (!check_runtime()) runTime = "60s";
-        }
-    } else{
-        update_password();
-    }
-    sudo("airmon-ng start " + getWifiCard() );
-
-    sudo("timeout "+ runTime + " airodump-ng -b a -I 10 -w " + resc + "/stored/out --berlin 60  " + getWifiCard());
-    // program              5ghz band, 1 second interface times, out-01.csv, 30 seconds before station is removed,
-
-    system("clear");
-
-    //Make sure this runs and only runs after &runTime  has passed
-    ifstream outfile(resc + "/stored/out-01.csv");
-
-    string trash;
-    string tempMac;
-    string power;
-
-    while(outfile && trash != "ESSIDs"){
-        outfile >> trash;
-    }
-    if (!outfile){
-        return finish(3);
-    }
-
-    //outfile should now read stations
-    map<string, int> scanned_devices; //mac address, power
-    airodump_read(scanned_devices, outfile);
-
-    ofstream output(resc + "/output.txt");
-    system("clear");
-    cout << "Finished scanning stations, now printing:" << endl;
-    int i = 0;
-        vector<pair<string , int>> known_scanned;
-    for(const auto& item : scanned_devices){
-        cout << i << "m= " << item.first << endl;
-        cout << i << "p= " << item.second << endl;
-        cout << endl;
-        ++i;
-        if (!known_devices[item.first].empty()){
-            known_scanned.emplace_back(item);
+// initializes (std::map<string,string> devices)
+void makeKnownMap(){
+    ifstream known(resc + "known.txt");
+    while(known){
+        string tempName;
+        string fullName;
+        known >> tempName;
+        if (!known) break;
+        if (tempName.substr(tempName.size() - 2) == "n="){
+            known >> tempName;
+            while(tempName.substr(tempName.size() - 2) != "m="){
+                fullName += tempName + " ";
+                known >>tempName;
+            }
+            string tempMac;
+            known >> tempMac;
+            known_devices[tempMac] = fullName;
         }
     }
-    cout << endl;
-    cout << "Known devices present:" << endl;
-    if (!known_scanned.empty()){
-        for(const auto& item : known_scanned){
-            cout << "Name: ";
-            string tname =  known_devices[item.first];
-            cout << tname << endl;
-            cout << "Power: ";
-            int tpower = item.second;
-            cout << tpower << endl << endl;
+}//DT
 
-            output << "Name: " << tname << endl << "Power: " << tpower << endl << endl;
-        }
-    }
-    else{
-        cout << "None" << endl;
-        output << "None" << endl;
-    }
+//Requires "/" to be at the end of relative file location
+void generateConfigFile(const string & relative_file_location ){
+    ofstream optionsFile(relative_file_location + "mapper.conf");
+    optionsFile << "#\n#\nSOF\n";
+    optionsFile << "";
+}
 
+map<string, string> options;
 
-    return finish(0);
+//Requires "/" for  relative file location
+bool initOptions(){
+    ifstream input(program_root + "mapper.conf");
+    return false;
 }
 
 
+
+int main (int argc, char ** argv){
+    //Resc location
+    try{
+        initOptions();
+    }
+    catch (...) {
+        generateConfigFile("../");
+    }
+
+
+    //Wifi card
+
+
+
+
+
+    return 0;
+}
--- mapper.h
+++ mapper.h
@@ -8,153 +8,160 @@
 #include <cctype>
 #include <fstream>
 #include <cassert> 
-#include <algorithm> 
+#include <algorithm>
 #include <vector>
 #include <map>
 using namespace std;
 
 
-//GLOBALS --------------------------------------------------------------------------------------------------------------
-// Location of resc folder
-const string resc = "/home/loris/Projects/Mapper/resc";
-
-// amount of time that airodump will run (default is 60s)
-string runTime = "60s";
-
-string password;
-string user;
+//    const int a = 1;
+//    const int b = 2;
+//    int const   *  p = &a; // datatype * p = &a; datatype is const int: "int const" == "const int"
+//    p = &b; // Ok
+//    *p = 4; // Not Ok
 
 
-//Helper functions------------------------------------------------------------------------------------------------------
-
-map<string, string> known_devices; // known_devices[Mac] = Name
-
-//Changes a string to upper case
-string toUpperCase(string input){
-    transform(input.begin(), input.end(), input.begin(), ::toupper);
-    return input;
-}
-string toLowerCase(string input){
-    transform(input.begin(), input.end(), input.begin(), ::tolower);
-    return input;
-}
 
 
-//Checks whether or not string in is a mac adress
-bool isMacAddress(string in){
-    in = toUpperCase(in);
-    if(int(in.length()) != 17) return false;
-    for(int i = 0; i < int(in.length()); i++){
-        char temp = in[i];
-        if((i-2)%3 == 0 && temp != ':')return false;
-    }
-    return true;
-}
 
-//returns the mac adress without the colons
-string strip_colons(string mac){
-    //9A:65:33:17:49:EE
-    //012           2
-    //9A65:33:17:49:EE
-    //01234         4
-    //9A6533:17:49:EE
-    //0123456       6
-    //9A653317:49:EE
-    //012345678     8
-    //9A65331749:EE
-    //01234567890   10
-    mac = toUpperCase(mac);
-    if(isMacAddress(mac)){
-        for(int i = 2; i < 11; i+=2){
-            mac = mac.substr(0, i) + mac.substr(i+1);
+
+//Helper functions -----------------------------------------------------------------------------------------------------
+
+
+
+//Returns the input string in upper case
+string toUpperCase(const string & input){
+    string new_input = input;
+    transform(new_input.begin(), new_input.end(), new_input.begin(), ::toupper);
+    return new_input;
+}//D
+
+//Returns the input string in lower case
+string toLowerCase(const string & input){
+    string new_input = input;
+    transform(new_input.begin(), new_input.end(), new_input.begin(), ::tolower);
+    return new_input;
+}//D
+
+//runs a valid command with sudo before it
+void sudo(const string & command){
+    string out;
+    //out  =("echo ");
+    //out += password;
+    //out += (" | ");
+    out += ("sudo ");
+    //out += (" -S ");
+    out += command;
+    system(out.c_str());
+}//DT
+
+//Returns if a command is in the format "%whitespace sudo"
+bool isSudo(const string  & command){
+    size_t size = command.length();
+    for(size_t i = 0; i < size; ++i){
+
+        if(command[i] == 's'){
+            string comp = command.substr(i, 4);
+            return (comp == "sudo");
         }
+        if(command[i] != ' ' && command[i] != '\n') return false;
     }
-    return mac;
-}//Done!
+    return false;
+}//DT
 
 //Gets the output of a command, needs the command to be valid
 string getCommandOutput(string command){
     string tmpflname = "DeleteMe.txt";
-    command += " > " + tmpflname; 
-    system(command.c_str()); // cmd > Deleteme.txt
-    string out = "";
+    command += " > " + tmpflname;
+
+    system(command.c_str()); // %cmd > Deleteme.txt
+
+    string out;
     string temp;
     ifstream fin(tmpflname);
     for(int i = 0; fin >> temp; ++i) out += temp + " ";
     command = "rm -f " + tmpflname;
     system(command.c_str());
     return out;
-}
-
+}//DT
 
 //Writes a command to an output file
 void writeCommandOutputToFile(const string  & fileName, string command){
-    command += " > " + fileName; 
-    system(command.c_str()); // cmd > Deleteme.txt
-}
-
-//Gets the vendor of a mac address, needs a valid mac, Either with colons or without.
-string getVendor(string mac){
-    return getCommandOutput("/home/loris/Scripts/path/maclookup " + mac);
-}
+    command += " > " + fileName;
+    system(command.c_str()); // cmd > fileName
+}//DT
 
 
-//runs a valid command with sudo before it
-void sudo(const string & command){
-    string out;
-    out  =("echo "); 
-    out += password; 
-    out += (" | ");
-    out += ("sudo -S ");
-    out += command;
-    system(out.c_str());
-}
-void sudo(const char * command){
-    string out;
-    out  =("echo "); 
-    out += password; 
-    out += (" | ");
-    out += ("sudo -S ");
-    out += command;
-    system(out.c_str());
-}
 
-//gets the name of the wifi card (will always pick the first one (0 then 1 then 2...))
-string getWifiCard(){
-    string command = "ifconfig ";
-    string tmpflname = "DeleteMe.txt";
-    command += " > " + tmpflname; 
-    system(command.c_str()); // cmd > Deleteme.txt
-    string out;
-    string temp;
-    ifstream fin(tmpflname);
-    fin >> temp;
-    while(fin >> temp && temp.substr(0,4) != "wlan");
-    command = "rm -f " + tmpflname;
-    system(command.c_str());
-    return temp.substr(0, temp.length() - 1); // Removes the :
-}
 
-// initializes (std::map<string,string> devices)
-void make_known_map(){
-    ifstream known(resc + "/known.txt");
-    while(known){
-        string tempName;
-        string fullName;
-        known >> tempName;
-        if (!known) break;
-        if (tempName.substr(tempName.size() - 2) == "n="){
-            known >> tempName;
-            while(tempName.substr(tempName.size() - 2) != "m="){
-                fullName += tempName + " ";
-                known >>tempName;
-            }
-            string tempMac;
-            known >> tempMac;
-            known_devices[tempMac] = fullName;
+
+
+//Returns whether or not the mac address inputted is a mac address with colons
+static bool isMacAddressWithColons(const string & in){
+    string new_in = toUpperCase(in);
+    if(int(new_in.length()) != 17) return false;
+    for(int i = 0; i < int(new_in.length()); i++){
+        char temp = new_in[i];
+        if( (i-2)%3 == 0 && temp != ':'&& temp != ' ' )return false;
+    }
+    return true;
+}//D
+
+//Strips colons from a valid mac address with colons.
+static string stripColons(const string & mac){
+
+    /*9A:65:33:17:49:EE
+      012           2
+      9A65:33:17:49:EE
+      01234         4
+      9A6533:17:49:EE
+      0123456       6
+      9A653317:49:EE
+      012345678     8
+      9A65331749:EE
+      01234567890   10 */
+
+    string newMac = toUpperCase(mac);
+    if(isMacAddressWithColons(newMac)){
+        for(int i = 2; i < 11; i+=2){
+            newMac = newMac.substr(0, i) + newMac.substr(i + 1);
         }
     }
-}
+    return newMac;
+}//DT
+
+
+//Gets vendor of a mac address in colon or plain format
+string getVendor(const string & address){
+    //Only checks the first 6 char
+    string cmd = "grep " + stripColons(address).substr(0,6) + " /usr/share/nmap/nmap-mac-prefixes";
+
+    string out;
+    out = getCommandOutput(cmd);
+    //74AC5F Qiku Internet Network Scientific (Shenzhen)
+    //01234567
+    if(out.length() > 0) return out.substr(7);
+    return "No Vendor could be found!";
+}//DT
+
+//GLOBALS --------------------------------------------------------------------------------------------------------------
+// Location of resc folder
+const string program_root = "~/";
+const string resc = program_root + "resc/";
+map<string, string> known_devices; // known_devices[Mac] = Name
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
